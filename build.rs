eae24dd45d22a283fb232deb11d447221b692470
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2024-09-22 16:07:33 +0800

    test

--- build.rs
+++ build.rs
@@ -1,14 +1,19 @@
-use ptx_builder::error::Result;
-use ptx_builder::prelude::*;
+use cuda_builder::CudaBuilder;
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     // Workaround for "crate required to be available in rlib format" bug
-    std::env::set_var("CARGO_BUILD_PIPELINING", "false");
+    // std::env::set_var("CARGO_BUILD_PIPELINING", "true");
 
     // Help cargo find libcuda
-    println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64/");
+    // println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64/");
 
+    // let builder = Builder::new("core")?;
+    // CargoAdapter::with_env_var("KERNEL_PTX_PATH").build(builder);
 
-    let builder = Builder::new("core")?;
-    CargoAdapter::with_env_var("KERNEL_PTX_PATH").build(builder);
+    CudaBuilder::new("core")
+        .copy_to("core.ptx")
+        .build()
+        .unwrap();
+
+    Ok(())
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,16 +1,69 @@
-#![no_std]
 #![cfg_attr(
-    any(target_arch = "nvptx", target_arch = "nvptx64"),
-    feature(abi_ptx, stdsimd)
+    target_os = "cuda",
+    no_std,
+    feature(register_attr),
+    register_attr(nvvm_internal)
 )]
+#![allow(improper_ctypes_definitions)]
 
-use rustacuda_core::DevicePointer;
-use rustacuda_derive::DeviceCopy;
+use cuda_std::prelude::*;
+use cust::prelude::*;
+use cust::DeviceCopy;
+use cuda_std::thread::{block_dim_x, block_idx_x, thread_idx_x};
+use byteorder::{ByteOrder, LittleEndian};
 
-#[cfg(any(target_arch = "nvptx", target_arch = "nvptx64"))]
-mod kernel;
+// #[panic_handler]
+// fn panic(_: &::core::panic::PanicInfo) -> ! {
+//     unsafe { trap() }
+// }
 
-#[derive(DeviceCopy, Clone)]
+
+extern crate alloc;
+
+#[inline]
+fn add_u256(base: &[u8; 32], mut offset: u64) -> [u8; 32] {
+    let mut res = [0; 32];
+    for i in 0..4 {
+        let start = i * 8;
+        let end = (i + 1) * 8;
+        let base = LittleEndian::read_u64(&base[start..end]);
+        let (total, overflow) = base.overflowing_add(offset);
+        LittleEndian::write_u64(&mut res[start..end], total);
+        if overflow {
+            offset = 1;
+        } else {
+            offset = 0;
+        }
+    }
+    res
+}
+
+
+#[kernel]
+pub unsafe fn render(params_ptr: *mut KernelParams) {
+    let params = unsafe { &mut *params_ptr };
+    let x = unsafe { block_dim_x() * block_idx_x() + thread_idx_x() } as u64;
+
+    let seed = unsafe { core::slice::from_raw_parts(params.seed.as_ptr(), 32) }
+        .try_into()
+        .unwrap();
+    let cur_seed = add_u256(seed, x);
+    let s = ed25519_compact::Seed::new(cur_seed);
+    let kp = ed25519_compact::KeyPair::from_seed(s);
+
+    let byte_prefixes =
+        unsafe { core::slice::from_raw_parts_mut(params.byte_prefixes.as_mut_ptr(), params.byte_prefixes_len) };
+    for byte_prefix in byte_prefixes {
+        if byte_prefix.matches(&*kp.pk) {
+            let out = unsafe { core::slice::from_raw_parts_mut(byte_prefix.out.as_mut_ptr(), 32) };
+            out.clone_from_slice(&cur_seed);
+            let success = unsafe { &mut *byte_prefix.success.as_mut_ptr() };
+            *success = true;
+        }
+    }
+}
+
+#[derive(DeviceCopy, Copy, Clone)]
 #[repr(C)]
 pub struct KernelParams {
     pub seed: DevicePointer<u8>,
@@ -18,7 +71,7 @@ pub struct KernelParams {
     pub byte_prefixes_len: usize,
 }
 
-#[derive(DeviceCopy, Clone)]
+#[derive(DeviceCopy, Copy, Clone)]
 #[repr(C)]
 pub struct BytePrefix {
     pub byte_prefix: DevicePointer<u8>,
@@ -31,7 +84,7 @@ pub struct BytePrefix {
 impl BytePrefix {
     pub fn matches(&self, data: &[u8]) -> bool {
         let slice =
-            unsafe { core::slice::from_raw_parts(self.byte_prefix.as_raw(), self.byte_prefix_len) };
+            unsafe { core::slice::from_raw_parts(self.byte_prefix.as_ptr(), self.byte_prefix_len) };
         data.starts_with(&slice[..self.last_byte_idx])
             && data[self.last_byte_idx] & self.last_byte_mask == slice[self.last_byte_idx]
     }
